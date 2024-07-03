150835a9629a8423444220bf4dce68213bd4a6a1
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-07-03 17:41:00 +0500

    User main screen
    Updated User main screen code

--- lib/Screens/user_main_sc.dart
+++ lib/Screens/user_main_sc.dart
@@ -4,7 +4,10 @@ import 'package:get/get.dart';
 import 'package:secondevaluation/Screens/PrintShop/Customer_View.dart';
 import 'package:secondevaluation/Screens/complaint_sc.dart';
 import 'package:secondevaluation/Screens/home_sc.dart';
+import 'package:secondevaluation/widgets/bottom_nav_bar/cafe_nav_bar.dart';
 import 'package:secondevaluation/widgets/drawer.dart';
+
+import '../widgets/discount_carousel.dart';
 
 class UserMainScreen extends StatefulWidget {
   const UserMainScreen({super.key});
@@ -26,15 +29,12 @@ class _UserMainScreenState extends State<UserMainScreen> {
         iconTheme: const IconThemeData(color: Colors.white),
         elevation: 1,
         title: Text(
-            'Book & Bite',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white ),
-          textAlign: TextAlign.center ,
+          'Book & Bite',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white ),
+          textAlign: TextAlign.center,
         ),
-
       ),
-
       drawer: SideDrawer(),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -48,7 +48,7 @@ class _UserMainScreenState extends State<UserMainScreen> {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(right: 4.0),
-                        child: _buildButton('Cafeteria' ,  screenWidth * 0.85, screenHeight * 0.2),
+                        child: _buildButton('Cafeteria', screenWidth * 0.85, screenHeight * 0.2, 'assets/background/cafeteria.jpg'),
                       ),
                     ],
                   ),
@@ -58,26 +58,26 @@ class _UserMainScreenState extends State<UserMainScreen> {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(right: 3.0),
-                        child: _buildButton(
-                            'Printing \n Shop', screenWidth * 0.42, screenHeight * 0.15),
+                        child: _buildButton('Printing \n Shop', screenWidth * 0.42, screenHeight * 0.15, 'assets/background/printshop.jpg'),
                       ),
-                      _buildButton(
-                          'Complaints', screenWidth * 0.42, screenHeight * 0.15),
+                      _buildButton('Complaints', screenWidth * 0.42, screenHeight * 0.15, 'assets/background/complaints.jpg'),
                     ],
                   ),
                 ],
               ),
             ),
+            DiscountCarousel(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildButton(String text, double width, double height) {
-    return ElevatedButton(
-      onPressed: () {
+  Widget _buildButton(String text, double width, double height, String imagePath) {
+    return GestureDetector(
+      onTap: () {
         if (text == 'Cafeteria') {
+          // Get.to(CurvedNavigationBarExample());
           Get.to(Home_Sc());
         } else if (text == 'Printing \n Shop') {
           Get.to(CustomerView());
@@ -85,17 +85,32 @@ class _UserMainScreenState extends State<UserMainScreen> {
           Get.to(ComplaintScreen());
         }
       },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color.fromARGB(255, 109, 220, 217),
-        shadowColor: Color.fromARGB(255, 140, 245, 241),
-        minimumSize: Size(width, height),
-        shape: RoundedRectangleBorder(
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imagePath),
+            fit: BoxFit.cover,
+          ),
           borderRadius: BorderRadius.circular(10.0),
         ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 20, color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Row(
+            children: [
+              Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+            ],
+          ),
+        ),
 
       ),
     );
